aecc731bcb8aa077e2dd8b350ac7606e00b32b68
SunOcaGames <126535766+SunOcaGames@users.noreply.github.com>
2023-03-16 10:43:07 +0000

    Initial commit

--- addons/chrome.js
+++ addons/chrome.js
@@ -1,17 +1,22 @@
 // ==UserScript==
-// @name         scratch
+// @name         SOG-Chrome-Addons
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
+// @version      0.63
+// @description  太陽之海遊戲工作室的瀏覽器外掛公用程式
 // @author       Dong
-// @match        https://scratch.mit.edu/*/*
-// @icon         https://scratch.mit.edu/favicon.ico
+// @match        https://scratch.mit.edu/*
+// @icon         https://sunocagames.github.io/SunOcaGames/img/sun_logo_%E6%AD%A3%E6%96%B9%E5%BD%A2.png
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    setTimeout(() => {
+    setInterval(() => {
+      if ('https://scratch.mit.edu/'==location.href) {
         var namespan=document.getElementsByClassName('profile-name')
         console.log(namespan[0].innerText)
-    }, 5000);
+      } else if ('https://scratch.mit.edu/projects/'.indexOf(location.href)!=-1) {
+        namespan=document.getElementsByClassName('user-name')
+        console.log(namespan[0].innerText)
+      }
+    }, 1000);
 })();
--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-import server.servermain as sm
-sm.run()
+from flask import Flask
+from flask import render_template
+import server.firebase as fire
+import time as t
+
+app = Flask('app')
+
+
+@app.route('/')
+def home():
+  return '<h2>這裡是你該來的地方嗎！走錯路了啦！<br>😤🤬👾<br>這裡不是每個人都能來的！</h2>'
+
+
+@app.route('/online/<name>')
+def online(name):
+  fire.save(name)
+  return name + '在' + str(t.time()) + '時宣布在線，已上傳至firebase'
+
+
+@app.route('/test')
+def test():
+  data = fire.dowmdate('Scratch_Online/SOG')
+  return '上次驗證登入在' + format(t.time() - data['online_time'], '.2f') + '秒前'
+
+@app.route('/addons')
+def addons():
+  with open('addons/chrome.js','r') as f:
+    return f.read()
+app.run(host='0.0.0.0', port=8080)
