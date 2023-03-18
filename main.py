aab7eba891a984042a3dcd99f13547a21b6f5bb4
SunOcaGames <126535766+SunOcaGames@users.noreply.github.com>
2023-03-18 03:44:05 +0000

    加入了測試連結，把discordwebhook的url改放在firebase上了，把/test改成/WhoIsOnline了

--- main.py
+++ main.py
@@ -1,55 +1,77 @@
 from flask import Flask
+import server.discord_webhook as dc_wh
 import server.firebase as fire
 import time as t
 import datetime
 
 app = Flask('app')
 
+
 def AddLog(text):
-  with open('server/日誌.txt','a') as flog:
-    nowtime= datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=8)))
-    return flog.write(str(nowtime).split('+')[0]+'：寫入日誌：'+text+'\n')
+  with open('server/日誌.txt', 'a') as flog:
+    nowtime = datetime.datetime.now(
+      tz=datetime.timezone(datetime.timedelta(hours=8)))
+    return flog.write(str(nowtime).split('+')[0] + '：寫入日誌：' + text + '\n')
+
 
 @app.route('/')
 def home():
   return '<h2>這裡是你該來的地方嗎！走錯路了啦！<br>😤🤬👾<br>這裡不是每個人都能來的！</h2>'
+
 
 @app.route('/UpTimeRobot')
 def UpTimeRobot():
   AddLog('url-/UpTimeRobot被開啟了')
   return 'Hello UpTimeRobot!'
 
+
 @app.route('/online/<name>')
 def online(name):
   AddLog('url-/online/<name>被開啟了')
   fire.save(name)
-  AddLog(name+'宣布在線，已上傳至firebase')
+  AddLog(name + '宣布在線，已上傳至firebase')
   return name + '在' + str(t.time()) + '時宣布在線，已上傳至firebase'
 
-
-@app.route('/test')
-def test():
-  AddLog('url-/test被開啟了')
+@app.route('/logut')
+def logut():
+  AddLog('/logut被開啟了')
   data = fire.dowmdate('Scratch_Online/SOG')
-  AddLog('已有人詢問有誰登入，已回復：上次驗證登入在' + format(t.time() - data['online_time'], '.2f') + '秒前')
+  fire.save('')
+  AddLog(data['name'] + '宣布離線，已上傳至firebase')
+  return data['name'] + '在' + str(t.time()) + '時宣布離線，已上傳至firebase'
+
+@app.route('/WhoIsOnline')
+def WhoIsOnline():
+  AddLog('url-/WhoIsOnline被開啟了')
+  data = fire.dowmdate('Scratch_Online/SOG')
+  AddLog('已有人詢問有誰登入，已回復：上次驗證登入在' +
+         format(t.time() - data['online_time'], '.2f') + '秒前')
   return '上次驗證登入在' + format(t.time() - data['online_time'], '.2f') + '秒前'
+
 
 @app.route('/addons')
 def addons():
   AddLog('url-/addons被開啟了')
   AddLog('已傳送addons/chrome.js檔')
-  with open('addons/chrome.js','r') as f:
+  with open('addons/chrome.js', 'r') as f:
     return f.read()
+
 
 @app.route('/log')
 def log():
   AddLog('url-/log被開啟了')
   AddLog('已傳送server/日誌.txt檔')
-  t.sleep(0.5)
-  with open('server/日誌.txt','r') as f:
-    a=''
+  t.sleep(1)
+  with open('server/日誌.txt', 'r') as f:
+    a = ''
     for line in f.readlines():
-      a=line+'<br>'+a
-    return '<h2>beta測試版</h2>太陽之海遊戲的chrome外掛程式伺服器的日誌'+a
-app.run(host='0.0.0.0', port=8080)
+      a = line + '<br>' + a
+    return '<h2>beta測試版</h2>太陽之海遊戲的chrome外掛程式伺服器的日誌<br>' + a
 
+@app.route('/test/dcwebhook/<title>/<description>')
+def test_dcwebhook(title,description):
+  AddLog('test/dcwebhook/<title>/<description>被開啟了')
+  AddLog('已使用discord webhook傳送{'+title+':'+description+'}')
+  dc_wh.post(title,description)
+  return '已發送'
+app.run(host='0.0.0.0', port=8080)
--- server/discord_webhook.py
+++ server/discord_webhook.py
@@ -1,4 +1,6 @@
 from discordwebhook import Discord
-discord = Discord( url="https://discord.com/api/webhooks/1085915907274051637/J5JO8IK757EEYpQAr8a_u2mZxi4jtk-lPrAsT8UhMb9F4TocyEWX502To_VLA_9O_xwh")
-while True:
-  discord.post(content=input('`:'))
+import server.firebase as fire
+dcurl=fire.dowmdate('Scratch_Online/SOG')['Discord_webhook']
+discord = Discord(url=dcurl)
+def post(title,description):
+  discord.post(embeds=[{"title":title,"description":description,'name':'SOG-Chrome-Addons'}])
